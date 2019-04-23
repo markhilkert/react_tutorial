9c1d06194204459e0a22a65bc5c6f813f4b25327
markhilkert <mark.hilkert@gmail.com>
2019-04-23 11:47:16 -0500

    intiial commit

---
+++ react_to_do_sample/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p> Take out the trash </p>
+      <input type="checkbox" />
+      <p> Do Coding </p>
+      <input type="checkbox" />
+      <p> Apply to Jobs </p>
+      <input type="checkbox" />
+      <p> Eat Food </p>
+    </div> 
+  );
+}
+
+export default App;
